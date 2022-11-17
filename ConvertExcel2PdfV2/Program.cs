b60b2c7856a450adcb492acb37f924648f5727d0
Josky-olo <56986562+Josky-olo@users.noreply.github.com>
2022-11-17 10:15:36 +0700

    Add project files.

---
+++ ConvertExcel2PdfV2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace ConvertExcel2PdfV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String workbookPath = "C:\\Users\\prasatphon\\Desktop\\a.xlsx";
+            String outputPath = "C:\\Users\\prasatphon\\Desktop\\b.pdf";
+            // If either required string is null or empty, stop and bail out
+            if (string.IsNullOrEmpty(workbookPath) || string.IsNullOrEmpty(outputPath))
+            {
+                System.Console.WriteLine("false ja");
+            }
+
+            // Create COM Objects
+            Microsoft.Office.Interop.Excel.Application excelApplication;
+            Microsoft.Office.Interop.Excel.Workbook excelWorkbook;
+
+            // Create new instance of Excel
+            excelApplication = new Microsoft.Office.Interop.Excel.Application();
+
+            // Make the process invisible to the user
+            excelApplication.ScreenUpdating = false;
+
+            // Make the process silent
+            excelApplication.DisplayAlerts = false;
+
+
+            // Open the workbook that you wish to export to PDF
+            excelWorkbook = excelApplication.Workbooks.Open(workbookPath);
+
+            // If the workbook failed to open, stop, clean up, and bail out
+            if (excelWorkbook == null)
+            {
+                excelApplication.Quit();
+
+                excelApplication = null;
+                excelWorkbook = null;
+
+                System.Console.WriteLine("false ja");
+            }
+
+            var exportSuccessful = true;
+            try
+            {
+                // Call Excel's native export function (valid in Office 2007 and Office 2010, AFAIK)
+                excelWorkbook.ExportAsFixedFormat(Microsoft.Office.Interop.Excel.XlFixedFormatType.xlTypePDF, outputPath);
+            }
+            catch (System.Exception ex)
+            {
+                // Mark the export as failed for the return value...
+                exportSuccessful = false;
+
+                // Do something with any exceptions here, if you wish...
+                // MessageBox.Show...        
+            }
+            finally
+            {
+                // Close the workbook, quit the Excel, and clean up regardless of the results...
+                excelWorkbook.Close();
+                excelApplication.Quit();
+
+                excelApplication = null;
+                excelWorkbook = null;
+            }
+
+            // You can use the following method to automatically open the PDF after export if you wish
+            // Make sure that the file actually exists first...
+            if (System.IO.File.Exists(outputPath))
+            {
+                System.Diagnostics.Process.Start(outputPath);
+            }
+        }
+    }
+}
